61f55994e4ba24476f7491fa90c0a8c9a1659539
Amy Aikawa <bupmaster@Amys-MacBook-Air.local>
2023-10-02 11:45:44 -0400

    Added do now 10/2/23

---
+++ do now.py
@@ -0,0 +1,17 @@
+def main():
+    flowers = 5
+    trees = 10
+    plants = flowers + trees
+    trees = 20
+    print(plants)
+
+    number1 = 125
+    number2 = 100
+    number3 = number1 - number2
+    number3 = 10
+    number1 = 20
+    number2 = number1 + number2 + number3
+    print(number2)
+
+if __name__ == "__main__":
+    main()
