9823b051be949ca97f1aea55b4f7f6d5b2b2be9e
Amy Aikawa <bupmaster@Amys-MacBook-Air.local>
2023-10-04 11:18:38 -0400

    Added add_numbers.py

---
+++ add_numbers.py
@@ -0,0 +1,13 @@
+def main():
+
+    number1 = 125
+    number2 = 100
+    number3 = number1 - number2
+    print(number3)
+    number3 = 10
+    number1 = 20
+    number2 = number1 + number2 + number3
+    print(number3 + number2)
+
+if __name__ == "__main__":
+    main()
