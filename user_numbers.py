0ff28bb9a5f8a3dafae5bda6c473e3da1320191a
Amy Aikawa <bupmaster@Amys-MacBook-Air.local>
2023-10-04 12:13:26 -0400

    Added user_numbers.py

---
+++ user_numbers.py
@@ -0,0 +1,12 @@
+def main():
+    print("This program adds two numbers.")
+
+    number1 = input("Enter the first number: ")
+    number2 = input("Enter the second number: ")
+
+    total = int(number1) + int(number2)
+
+    print("The total is " + str(total) + ".")
+
+if __name__ == "__main__":
+    main()
