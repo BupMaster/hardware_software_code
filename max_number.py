35c20688cb0c1a98abc1a6230d4d081594e68079
Amy Aikawa <bupmaster@Amys-MacBook-Air.local>
2023-10-16 11:49:02 -0400

    Added max_number.py

---
+++ max_number.py
@@ -0,0 +1,12 @@
+def max(num1, num2):
+    if num1 >= num2:
+        return num1
+    return num2
+
+def main():
+    larger = max(1, 3)
+    print(larger)
+
+
+if __name__ == "__main__":
+    main()
