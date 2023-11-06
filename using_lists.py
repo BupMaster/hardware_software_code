1566f60841bd0911c9e7aab520b017ca2e0e6435
Amy Aikawa <bupmaster@Amys-MacBook-Air.local>
2023-11-06 11:05:32 -0500

    Added using_lists.py

---
+++ using_lists.py
@@ -0,0 +1,11 @@
+def main():
+    nums = [3, 41, 12, 9, 74, 15]
+    print(len(nums))
+    print(max(nums))
+    print(min(nums))
+    print(sum(nums))
+    avg = (sum(nums)/len(nums))
+    print(avg)
+    
+if __name__ == "__main__":
+    main()
