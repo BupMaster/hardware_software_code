69548ebf2aa403a74d08cf965b724f6c9f6143b1
Amy Aikawa <bupmaster@Amys-MacBook-Air.local>
2023-12-06 11:56:44 -0500

    Fixing class_dictionary.py

--- class_dictionary.py
+++ class_dictionary.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-def display_menu(num):
-    menu_dict = {
-        '1' : 'Apples',
-        '2' : 'Pears',
-        '3' : 'Bananas',
-    }
-    for items, values in menu_dict() .items():
-        print(items+". "+ values)
-
-def main():
-    display_menu()
-=======
 def display_menu():
     menu_dict = {
         '1': 'Apples',
@@ -24,4 +11,3 @@ def main():
     display_menu()
 
 main()
->>>>>>> refs/remotes/origin/main
